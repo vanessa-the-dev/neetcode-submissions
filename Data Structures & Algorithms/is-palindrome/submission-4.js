4f5ae81a38bea71e5b14395475c5be94f93d8a92
Vanessa Isidro <nellyn.isidro@gmail.com>
2026-09-03 13:24:45 +0900

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let l = 0;
+        let r = s.length - 1;
+
+        const str = s.toLowerCase();
+
+        while (l < r) {
+            while (l < r && !this.isAlphanumeric(str[l])) {
+                l++;
+            }
+
+            while (r > l && !this.isAlphanumeric(str[r])) { 
+                r--;
+            }
+
+            if (str[l] !== str[r]) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    isAlphanumeric(c) {
+        return (
+            (c >= 'a' && c <= 'z') ||
+            (c >= '0' && c <= '9')
+        );
+    }
+}
