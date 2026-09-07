1d02294abe258c715575c83342644dd6b54d4e6a
Vanessa Isidro <nellyn.isidro@gmail.com>
2026-09-07 15:45:16 +0900

    Update: products-of-array-discluding-self - submission-1

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -5,17 +5,18 @@ class Solution {
      */
     productExceptSelf(nums) {
         const n = nums.length;
-        const list = new Array(n).fill(1);
+        const res = Array(n).fill(1);
 
         for (let i = 1; i < n; i++) {
-            list[i] = list[i-1] * nums[i-1];
+            res[i] = res[i-1] * nums[i-1];
         }
 
         let postfix = 1;
         for (let i = n - 1; i >= 0; i--) {
-            list[i] *= postfix;
+            res[i] *= postfix;
             postfix *= nums[i];
         }
-        return list;
+
+        return res;
     }
 }
