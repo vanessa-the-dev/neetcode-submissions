6cb9db08fcd5de2497a728a8141a169fa8b5918c
Vanessa Isidro <nellyn.isidro@gmail.com>
2026-09-24 13:18:11 +0900

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        const map = new Map();
+
+        for (let char of s) {
+            map.set(char, (map.get(char) || 0) + 1);
+        }
+
+        for (let char of t) {
+            let charCnt = map.get(char) || 0;
+
+            if (charCnt === 0) {
+                return false;
+            }
+
+            map.set(char, map.get(char) - 1);
+        }
+
+        return true;
+    }
+}
